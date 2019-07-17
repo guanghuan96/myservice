a4eaacb14c1e9ffaab66bf55063a61a288a828b3
guanghuan96 <52998135+guanghuan96@users.noreply.github.com>
2019-07-17 22:28:48 +0800

    master update 01

--- src/test/test1.java
+++ src/test/test1.java
@@ -36,7 +36,7 @@ public class test1 {
 	      int rows=session.insert("test.insert", stu);
 	      System.out.println(stu.getStuid());
 	      session.commit();
-	      System.out.println("aaaa");
+	      System.out.println("aaCCCCaa");
 	    is.close();
 	}
 }
